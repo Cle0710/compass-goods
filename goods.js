6a95feb86d929318c768a266444e4a927f4bf14f
Cle0710 <74m9g9vhb7@privaterelay.appleid.com>
2026-07-04 05:48:56 +0900

    Create goods.js

---
+++ goods.js
@@ -0,0 +1,27 @@
+const goods = [
+  {
+  id: "0001-01",
+  name: "ウエハースカード 十文字アタリ",
+  type: "カード",
+  image: "images/IMG_4559.jpeg",
+  seriesId: "0001",
+  heroTags: [1]
+  },
+
+  {
+    id: "0001-02",
+    name: "ウエハースカードホロ 十文字アタリ",
+  type: "カード",
+  image: "",
+  seriesId: "0001",
+    heroTags: [1]
+  },
+  {
+    id: "0001-03",
+    name: "ウエハース特典アタリ,ジャスティス,リリカ,乃保",
+    type: "カード",
+    image: "",
+    seriesId: "0001",
+    heroTags: [1, 2, 3, 4]
+  }
+];
