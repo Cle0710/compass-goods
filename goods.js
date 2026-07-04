9ecb2bdc2dfece5f0157cdca6953303174406142
Cle0710 <74m9g9vhb7@privaterelay.appleid.com>
2026-07-04 10:51:01 +0900

    Update goods.js

--- goods.js
+++ goods.js
@@ -3,11 +3,10 @@ const goods = [
   id: "0001-01",
   name: "ウエハースカード 十文字アタリ",
   type: "カード",
-  image: "images/IMG_4559.jpeg",
+  image: "",
   seriesId: "0001",
   heroTags: [1]
   },
-
   {
     id: "0001-02",
     name: "ウエハースカードホロ 十文字アタリ",
@@ -23,5 +22,37 @@ const goods = [
     image: "",
     seriesId: "0001",
     heroTags: [1, 2, 3, 4]
-  }
+  },
+  {
+    id: "0001-04",
+    name: "ウエハースカード ジャスティス ハンコック",
+  type: "カード",
+  image: "",
+  seriesId: "0001",
+    heroTags: [2]
+  },
+  {
+    id: "0001-05",
+    name: "ウエハースカード ホロ ジャスティス ハンコック",
+  type: "カード",
+  image: "",
+  seriesId: "0001",
+    heroTags: [2]
+  },
+  {
+    id: "0001-06",
+    name: "ウエハースカード リリカ",
+  type: "カード",
+  image: "",
+  seriesId: "0001",
+    heroTags: [3]
+  },
+  {
+    id: "0001-07",
+    name: "ウエハースカード ホロ リリカ",
+  type: "カード",
+  image: "",
+  seriesId: "0001",
+    heroTags: [3]
+  },
 ];
