5c12cbadda42cd4b21812ee97e41f4d4ca1b0735
Cle0710 <74m9g9vhb7@privaterelay.appleid.com>
2026-07-05 21:36:27 +0900

    Update goods.js

--- goods.js
+++ goods.js
@@ -1,6 +1,6 @@
 const goods = [
   {
-  id: "0001-01",
+  id: "0001-001",
   name: "ウエハースカード 十文字アタリ",
   type: "カード",
   image: "",
@@ -8,23 +8,15 @@ const goods = [
   heroTags: [1]
   },
   {
-    id: "0001-02",
-    name: "ウエハースカードホロ 十文字アタリ",
+    id: "0001-002",
+    name: "ウエハースカード ホロ 十文字アタリ",
   type: "カード",
   image: "",
   seriesId: "0001",
     heroTags: [1]
   },
   {
-    id: "0001-03",
-    name: "ウエハース特典アタリ,ジャスティス,リリカ,乃保",
-    type: "カード",
-    image: "",
-    seriesId: "0001",
-    heroTags: [1, 2, 3, 4]
-  },
-  {
-    id: "0001-04",
+    id: "0001-003",
     name: "ウエハースカード ジャスティス ハンコック",
   type: "カード",
   image: "",
@@ -32,7 +24,7 @@ const goods = [
     heroTags: [2]
   },
   {
-    id: "0001-05",
+    id: "0001-004",
     name: "ウエハースカード ホロ ジャスティス ハンコック",
   type: "カード",
   image: "",
@@ -40,7 +32,7 @@ const goods = [
     heroTags: [2]
   },
   {
-    id: "0001-06",
+    id: "0001-005",
     name: "ウエハースカード リリカ",
   type: "カード",
   image: "",
@@ -48,11 +40,731 @@ const goods = [
     heroTags: [3]
   },
   {
-    id: "0001-07",
+    id: "0001-006",
     name: "ウエハースカード ホロ リリカ",
   type: "カード",
   image: "",
   seriesId: "0001",
     heroTags: [3]
+  },
+  {
+    id: "0001-007",
+    name: "ウエハースカード 双挽 乃保",
+  type: "カード",
+  image: "",
+  seriesId: "0001",
+    heroTags: [4]
+  },
+  {
+    id: "0001-008",
+    name: "ウエハースカード ホロ 双挽 乃保",
+  type: "カード",
+  image: "",
+  seriesId: "0001",
+    heroTags: [4]
+  },
+  {
+    id: "0001-009",
+    name: "ウエハースカード 桜華 忠臣",
+    type: "カード",
+    image: "",
+    seriesId: "0001",
+    heroTags: [5]
+  },
+  {
+    id: "0001-010",
+    name: "ウエハースカード ホロ 桜華 忠臣",
+    type: "カード",
+    image: "",
+    seriesId: "0001",
+    heroTags: [5]
+  },
+  {
+    id: "0001-011",
+    name: "ウエハースカード ジャンヌ ダルク",
+    type: "カード",
+    image: "",
+    seriesId: "0001",
+    heroTags: [6]
+  },
+  {
+    id: "0001-012",
+    name: "ウエハースカード ホロ ジャンヌ ダルク",
+    type: "カード",
+    image: "",
+    seriesId: "0001",
+    heroTags: [6]
+  },
+  {
+    id: "0001-013",
+    name: "ウエハースカード マルコス'55",
+    type: "カード",
+    image: "",
+    seriesId: "0001",
+    heroTags: [7]
+  },
+  {
+    id: "0001-014",
+    name: "ウエハースカード ホロ マルコス'55",
+    type: "カード",
+    image: "",
+    seriesId: "0001",
+    heroTags: [7]
+  },
+  {
+    id: "0001-015",
+    name: "ウエハースカード ルチアーノ",
+    type: "カード",
+    image: "",
+    seriesId: "0001",
+    heroTags: [8]
+  },
+  {
+    id: "0001-016",
+    name: "ウエハースカード ホロ ルチアーノ",
+    type: "カード",
+    image: "",
+    seriesId: "0001",
+    heroTags: [8]
+  },
+  {
+    id: "0001-017",
+    name: "ウエハースカード Voidoll",
+    type: "カード",
+    image: "",
+    seriesId: "0001",
+    heroTags: [9]
+  },
+  {
+    id: "0001-018",
+    name: "ウエハースカード ホロ Voidoll",
+    type: "カード",
+    image: "",
+    seriesId: "0001",
+    heroTags: [9]
+  },
+  {
+    id: "0001-019",
+    name: "ウエハースカード 深川 まとい",
+    type: "カード",
+    image: "",
+    seriesId: "0001",
+    heroTags: [10]
+  },
+  {
+    id: "0001-020",
+    name: "ウエハースカード ホロ 深川 まとい",
+    type: "カード",
+    image: "",
+    seriesId: "0001",
+    heroTags: [10]
+  },
+  {
+    id: "0001-021",
+    name: "ウエハースカード グスタフ ハイドリヒ",
+    type: "カード",
+    image: "",
+    seriesId: "0001",
+    heroTags: [11]
+  },
+  {
+    id: "0001-022",
+    name: "ウエハースカード ホロ グスタフ ハイドリヒ",
+    type: "カード",
+    image: "",
+    seriesId: "0001",
+    heroTags: [11]
+  },
+  {
+    id: "0001-023",
+    name: "ウエハースカード ニコラ テスラ",
+    type: "カード",
+    image: "",
+    seriesId: "0001",
+    heroTags: [12]
+  },
+  {
+    id: "0001-024",
+    name: "ウエハースカード ホロ ニコラ テスラ",
+    type: "カード",
+    image: "",
+    seriesId: "0001",
+    heroTags: [12]
+  },
+  {
+    id: "0001-025",
+    name: "ウエハースカード ヴィオレッタ ノワール",
+    type: "カード",
+    image: "",
+    seriesId: "0001",
+    heroTags: [13]
+  },
+  {
+    id: "0001-026",
+    name: "ウエハースカード ホロ ヴィオレッタ ノワール",
+    type: "カード",
+    image: "",
+    seriesId: "0001",
+    heroTags: [13]
+  },
+  {
+    id: "0001-027",
+    name: "ウエハースカード コクリコット ブランシュ",
+    type: "カード",
+    image: "",
+    seriesId: "0001",
+    heroTags: [14]
+  },
+  {
+    id: "0001-028",
+    name: "ウエハースカード ホロ コクリコット ブランシュ",
+    type: "カード",
+    image: "",
+    seriesId: "0001",
+    heroTags: [14]
+  },
+  {
+    id: "0001-029",
+    name: "ウエハースカード マリア＝S＝レオンブルク",
+    type: "カード",
+    image: "",
+    seriesId: "0001",
+    heroTags: [15]
+  },
+  {
+    id: "0001-030",
+    name: "ウエハースカード ホロ マリア＝S＝レオンブルク",
+    type: "カード",
+    image: "",
+    seriesId: "0001",
+    heroTags: [15]
+  },
+  {
+    id: "0001-031",
+    name: "ウエハースカード アダム＝ユーリエフ",
+    type: "カード",
+    image: "",
+    seriesId: "0001",
+    heroTags: [16]
+  },
+  {
+    id: "0001-032",
+    name: "ウエハースカード ホロ アダム＝ユーリエフ",
+    type: "カード",
+    image: "",
+    seriesId: "0001",
+    heroTags: [16]
+  },
+  {
+    id: "0001-033",
+    name: "ウエハースカード 13†サーティーン†",
+    type: "カード",
+    image: "",
+    seriesId: "0001",
+    heroTags: [17]
+  },
+  {
+    id: "0001-034",
+    name: "ウエハースカード ホロ 13†サーティーン†",
+    type: "カード",
+    image: "",
+    seriesId: "0001",
+    heroTags: [17]
+  },
+  {
+    id: "0001-035",
+    name: "ウエハースカード かけだし勇者",
+    type: "カード",
+    image: "",
+    seriesId: "0001",
+    heroTags: [18]
+  },
+  {
+    id: "0001-036",
+    name: "ウエハースカード ホロ かけだし勇者",
+    type: "カード",
+    image: "",
+    seriesId: "0001",
+    heroTags: [18]
+  },
+  {
+    id: "0001-037",
+    name: "ウエハースカード メグメグ",
+    type: "カード",
+    image: "",
+    seriesId: "0001",
+    heroTags: [19]
+  },
+  {
+    id: "0001-038",
+    name: "ウエハースカード ホロ メグメグ",
+    type: "カード",
+    image: "",
+    seriesId: "0001",
+    heroTags: [19]
+  },
+  {
+    id: "0001-039",
+    name: "ウエハースカード イスタカ",
+    type: "カード",
+    image: "",
+    seriesId: "0001",
+    heroTags: [20]
+  },
+  {
+    id: "0001-040",
+    name: "ウエハースカード ホロ イスタカ",
+    type: "カード",
+    image: "",
+    seriesId: "0001",
+    heroTags: [20]
+  },
+  {
+    id: "0001-041",
+    name: "ウエハースカード 輝龍院 きらら",
+    type: "カード",
+    image: "",
+    seriesId: "0001",
+    heroTags: [21]
+  },
+  {
+    id: "0001-042",
+    name: "ウエハースカード ホロ 輝龍院 きらら",
+    type: "カード",
+    image: "",
+    seriesId: "0001",
+    heroTags: [21]
+  },
+  {
+    id: "0001-043",
+    name: "ウエハースカード ヴィーナス ポロロッチョ",
+    type: "カード",
+    image: "",
+    seriesId: "0001",
+    heroTags: [22]
+  },
+  {
+    id: "0001-044",
+    name: "ウエハースカード ホロ ヴィーナス ポロロッチョ",
+    type: "カード",
+    image: "",
+    seriesId: "0001",
+    heroTags: [22]
+  },
+  {
+    id: "0001-045",
+    name: "ウエハースカード ソーン＝ユーリエフ",
+    type: "カード",
+    image: "",
+    seriesId: "0001",
+    heroTags: [23]
+  },
+  {
+    id: "0001-046",
+    name: "ウエハースカード ホロ ソーン＝ユーリエフ",
+    type: "カード",
+    image: "",
+    seriesId: "0001",
+    heroTags: [23]
+  },
+  {
+    id: "0001-047",
+    name: "ウエハースカード デビルミント鬼龍 デルミン",
+    type: "カード",
+    image: "",
+    seriesId: "0001",
+    heroTags: [24]
+  },
+  {
+    id: "0001-048",
+    name: "ウエハースカード ホロ デビルミント鬼龍 デルミン",
+    type: "カード",
+    image: "",
+    seriesId: "0001",
+    heroTags: [24]
+  },
+  {
+    id: "0001-049",
+    name: "ウエハースカード トマス",
+    type: "カード",
+    image: "",
+    seriesId: "0001",
+    heroTags: [25]
+  },
+  {
+    id: "0001-050",
+    name: "ウエハースカード ホロ トマス",
+    type: "カード",
+    image: "",
+    seriesId: "0001",
+    heroTags: [25]
+  },
+  {
+    id: "0001-051",
+    name: "ウエハースカード 零夜",
+    type: "カード",
+    image: "",
+    seriesId: "0001",
+    heroTags: [26]
+  },
+  {
+    id: "0001-052",
+    name: "ウエハースカード ホロ 零夜",
+    type: "カード",
+    image: "",
+    seriesId: "0001",
+    heroTags: [26]
+  },
+  {
+    id: "0001-053",
+    name: "ウエハースカード ルルカ",
+    type: "カード",
+    image: "",
+    seriesId: "0001",
+    heroTags: [27]
+  },
+  {
+    id: "0001-054",
+    name: "ウエハースカード ホロ ルルカ",
+    type: "カード",
+    image: "",
+    seriesId: "0001",
+    heroTags: [27]
+  },
+  {
+    id: "0001-055",
+    name: "ウエハースカード ピエール 77世",
+    type: "カード",
+    image: "",
+    seriesId: "0001",
+    heroTags: [28]
+  },
+  {
+    id: "0001-056",
+    name: "ウエハースカード ホロ ピエール 77世",
+    type: "カード",
+    image: "",
+    seriesId: "0001",
+    heroTags: [28]
+  },
+  {
+    id: "0001-057",
+    name: "ウエハースカード 狐ヶ咲 甘色",
+    type: "カード",
+    image: "",
+    seriesId: "0001",
+    heroTags: [29]
+  },
+  {
+    id: "0001-058",
+    name: "ウエハースカード ホロ 狐ヶ咲 甘色",
+    type: "カード",
+    image: "",
+    seriesId: "0001",
+    heroTags: [29]
+  },
+  {
+    id: "0001-059",
+    name: "ウエハースカード HM-WA100 ニーズヘッグ",
+    type: "カード",
+    image: "",
+    seriesId: "0001",
+    heroTags: [30]
+  },
+  {
+    id: "0001-060",
+    name: "ウエハースカード ホロ HM-WA100 ニーズヘッグ",
+    type: "カード",
+    image: "",
+    seriesId: "0001",
+    heroTags: [30]
+  },
+  {
+    id: "0001-061",
+    name: "ウエハースカード ゲームバズーカガール",
+    type: "カード",
+    image: "",
+    seriesId: "0001",
+    heroTags: [31]
+  },
+  {
+    id: "0001-062",
+    name: "ウエハースカード ホロ ゲームバズーカガール",
+    type: "カード",
+    image: "",
+    seriesId: "0001",
+    heroTags: [31]
+  },
+  {
+    id: "0001-063",
+    name: "ウエハースカード 青春 アリス",
+    type: "カード",
+    image: "",
+    seriesId: "0001",
+    heroTags: [32]
+  },
+  {
+    id: "0001-064",
+    name: "ウエハースカード ホロ 青春 アリス",
+    type: "カード",
+    image: "",
+    seriesId: "0001",
+    heroTags: [32]
+  },
+  {
+    id: "0001-065",
+    name: "ウエハースカード イグニス＝ウィル＝ウィスプ",
+    type: "カード",
+    image: "",
+    seriesId: "0001",
+    heroTags: [33]
+  },
+  {
+    id: "0001-066",
+    name: "ウエハースカード ホロ イグニス＝ウィル＝ウィスプ",
+    type: "カード",
+    image: "",
+    seriesId: "0001",
+    heroTags: [33]
+  },
+  {
+    id: "0001-067",
+    name: "ウエハースカード 糸廻 輪廻",
+    type: "カード",
+    image: "",
+    seriesId: "0001",
+    heroTags: [34]
+  },
+  {
+    id: "0001-068",
+    name: "ウエハースカード ホロ 糸廻 輪廻",
+    type: "カード",
+    image: "",
+    seriesId: "0001",
+    heroTags: [34]
+  },
+  {
+    id: "0001-069",
+    name: "ウエハースカード Bugdoll",
+    type: "カード",
+    image: "",
+    seriesId: "0001",
+    heroTags: [35]
+  },
+  {
+    id: "0001-070",
+    name: "ウエハースカード ホロ Bugdoll",
+    type: "カード",
+    image: "",
+    seriesId: "0001",
+    heroTags: [35]
+  },
+  {
+    id: "0001-071",
+    name: "ウエハースカード ステリア・ララ・シルワ",
+    type: "カード",
+    image: "",
+    seriesId: "0001",
+    heroTags: [36]
+  },
+  {
+    id: "0001-072",
+    name: "ウエハースカード ホロ ステリア・ララ・シルワ",
+    type: "カード",
+    image: "",
+    seriesId: "0001",
+    heroTags: [36]
+  },
+  {
+    id: "0001-073",
+    name: "ウエハースカード ラヴィ・シュシュマルシュ",
+    type: "カード",
+    image: "",
+    seriesId: "0001",
+    heroTags: [37]
+  },
+  {
+    id: "0001-074",
+    name: "ウエハースカード ホロ ラヴィ・シュシュマルシュ",
+    type: "カード",
+    image: "",
+    seriesId: "0001",
+    heroTags: [37]
+  },
+  {
+    id: "0001-075",
+    name: "ウエハースカード アル・ダハブ＝アルカティア",
+    type: "カード",
+    image: "",
+    seriesId: "0001",
+    heroTags: [38]
+  },
+  {
+    id: "0001-076",
+    name: "ウエハースカード ホロ アル・ダハブ＝アルカティア",
+    type: "カード",
+    image: "",
+    seriesId: "0001",
+    heroTags: [38]
+  },
+  {
+    id: "0001-077",
+    name: "ウエハースカード 天空王 ぶれいずどらごん",
+    type: "カード",
+    image: "",
+    seriesId: "0001",
+    heroTags: [39]
+  },
+  {
+    id: "0001-078",
+    name: "ウエハースカード ホロ 天空王 ぶれいずどらごん",
+    type: "カード",
+    image: "",
+    seriesId: "0001",
+    heroTags: [39]
+  },
+  {
+    id: "0001-079",
+    name: "ウエハースカード 某〈なにがし〉",
+    type: "カード",
+    image: "",
+    seriesId: "0001",
+    heroTags: [40]
+  },
+  {
+    id: "0001-080",
+    name: "ウエハースカード ホロ 某〈なにがし〉",
+    type: "カード",
+    image: "",
+    seriesId: "0001",
+    heroTags: [40]
+  },
+  {
+    id: "0001-081",
+    name: "ウエハースカード クー・シー",
+    type: "カード",
+    image: "",
+    seriesId: "0001",
+    heroTags: [41]
+  },
+  {
+    id: "0001-082",
+    name: "ウエハースカード ホロ クー・シー",
+    type: "カード",
+    image: "",
+    seriesId: "0001",
+    heroTags: [41]
+  },
+  {
+    id: "0001-083",
+    name: "ウエハースカード アミスター＝バランディン",
+    type: "カード",
+    image: "",
+    seriesId: "0001",
+    heroTags: [42]
+  },
+  {
+    id: "0001-084",
+    name: "ウエハースカード ホロ アミスター＝バランディン",
+    type: "カード",
+    image: "",
+    seriesId: "0001",
+    heroTags: [42]
+  },
+  {
+    id: "0001-085",
+    name: "ウエハースカード 鬼ヶ式 うら",
+    type: "カード",
+    image: "",
+    seriesId: "0001",
+    heroTags: [43]
+  },
+  {
+    id: "0001-086",
+    name: "ウエハースカード ホロ 鬼ヶ式 うら",
+    type: "カード",
+    image: "",
+    seriesId: "0001",
+    heroTags: [43]
+  },
+  {
+    id: "0001-087",
+    name: "ウエハースカード コラプス",
+    type: "カード",
+    image: "",
+    seriesId: "0001",
+    heroTags: [44]
+  },
+  {
+    id: "0001-088",
+    name: "ウエハースカード ホロ コラプス",
+    type: "カード",
+    image: "",
+    seriesId: "0001",
+    heroTags: [44]
+  },
+  {
+    id: "0001-089",
+    name: "ウエハースカード みりぽゆ",
+    type: "カード",
+    image: "",
+    seriesId: "0001",
+    heroTags: [45]
+  },
+  {
+    id: "0001-090",
+    name: "ウエハースカード ホロ みりぽゆ",
+    type: "カード",
+    image: "",
+    seriesId: "0001",
+    heroTags: [45]
+  },
+  {
+    id: "0001-091",
+    name: "ウエハースカード チーちゃん",
+    type: "カード",
+    image: "",
+    seriesId: "0001",
+    heroTags: [46]
+  },
+  {
+    id: "0001-092",
+    name: "ウエハースカード ホロ チーちゃん",
+    type: "カード",
+    image: "",
+    seriesId: "0001",
+    heroTags: [46]
+  },
+  {
+    id: "0001-093",
+    name: "ウエハースカード ペルリニエ",
+    type: "カード",
+    image: "",
+    seriesId: "0001",
+    heroTags: [47]
+  },
+  {
+    id: "0001-094",
+    name: "ウエハースカード ホロ ペルリニエ",
+    type: "カード",
+    image: "",
+    seriesId: "0001",
+    heroTags: [47]
+  },
+  {
+    id: "0001-095",
+    name: "ウエハースカード メルーニャ",
+    type: "カード",
+    image: "",
+    seriesId: "0001",
+    heroTags: [48]
+  },
+  {
+    id: "0001-096",
+    name: "ウエハースカード ホロ メルーニャ",
+    type: "カード",
+    image: "",
+    seriesId: "0001",
+    heroTags: [48]
   },
 ];
